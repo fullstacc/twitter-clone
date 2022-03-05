9d79dcc67c4fda54ff296f525f64eed584acc1ec
sulfuroussquares <bbetancova@gmail.com>
2022-03-05 13:58:24 -0700

    Created initial view controller, landing page. Added assets

---
+++ Twitter/loginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  loginViewController.swift
+//  Twitter
+//
+//  Created by BRIAN BETANCOURT on 3/5/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
